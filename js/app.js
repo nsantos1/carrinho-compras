d0629ea82a4a681e8382e2838c804b46c61774be
nsantos1 <devnicolassantos@gmail.com>
2025-07-11 17:02:10 -0300

    adicionando function adicionar

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@ function adicionar(){
     //recuperar nome, quantiade e valor do produto
     let nomeValorProduto = document.getElementById('produto');
     let quantidade = document.getElementById('quantidade');
-
-    alert(nomeValorProduto.value);
-    alert(quantidade.value);
     //calcular  subtotal
+    
     //adicionar no carrinho
     //atualizar o valor total
 }
