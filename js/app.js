e66ae6d3f7b5591d9146f459b6b3f0296d8f3470
nsantos1 <devnicolassantos@gmail.com>
2025-07-11 23:12:58 -0300

    corrigindo commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 function adicionar(){
     //recuperar nome, quantiade e valor do produto
-    let nomeValorProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
+    let nomeValorProduto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
     //calcular  subtotal
-    
+    let valor = nomeValorProduto.match(/R\$(\d+)/)[1];
+    let produtoCarrinho = valor
+
+    console.log("Produto:", nomeValorProduto);
+    console.log("Valor unitário: R$", valor);
+    console.log("Quantidade:", quantidade);
+
     //adicionar no carrinho
     //atualizar o valor total
 }
